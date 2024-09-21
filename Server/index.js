56e003b4e665d54fba8998d505c0e33a4d49909d
Ajit raut <ajitraut9561@gmail.com>
2024-09-22 03:45:42 +0530

    indes.js updated

--- Server/index.js
+++ Server/index.js
@@ -3,17 +3,24 @@ const cors = require('cors');
 const connectDB = require('./Config/db');
 const userRoutes = require('./routes/authroute');
 const dotenv = require('dotenv');
+const categoryRoutes = require("./routes/Category");
+const bodyParser = require('body-parser');
+const path = require('path'); // Add this line to import the path module
+
 
 dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true })); 
 
 // Connect to MongoDB
 connectDB();
 
 // Use user routes
 app.use('/api/users', userRoutes);
+app.use('/uploads', express.static(path.join(__dirname, 'uploads'))); // Serve uploaded images
+app.use('/api/categories', categoryRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
