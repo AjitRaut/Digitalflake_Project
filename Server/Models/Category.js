fcd7befd9bf9f57d8bb79fd907f0747be44b974a
Ajit raut <ajitraut9561@gmail.com>
2024-09-22 03:42:46 +0530

    Addinng category scema

--- Server/Models/Category.js
+++ Server/Models/Category.js
@@ -1,8 +1,19 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
-const categorySchema = new mongoose.Schema({
-    id: Number,
-    name: String,
-    image: String,
-    status: String,
-  });
+const CategorySchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  status: {
+    type: String,
+    default: 'inactive', // Set default status to 'inactive'
+  },
+}, { timestamps: true });
+
+const Category = mongoose.model('Category', CategorySchema);
+module.exports = Category;
