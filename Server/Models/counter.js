c2a5a7ce97f7700261b0d803134bab1c21a978f5
Ajit raut <ajitraut9561@gmail.com>
2024-09-22 03:43:21 +0530

    Addinng counter

---
+++ Server/Models/counter.js
@@ -0,0 +1,10 @@
+// models/Counter.js
+const mongoose = require('mongoose');
+
+const counterSchema = new mongoose.Schema({
+  seq: { type: Number, default: 0 }
+});
+
+const Counter = mongoose.model('Counter', counterSchema);
+
+module.exports = Counter;
