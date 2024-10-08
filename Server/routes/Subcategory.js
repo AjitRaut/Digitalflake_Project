6b77abc000f8591f6157e7f4ddffd90ede8495fe
Ajit raut <ajitraut9561@gmail.com>
2024-10-08 22:31:15 +0530

    bug solved

--- Server/routes/Subcategory.js
+++ Server/routes/Subcategory.js
@@ -4,6 +4,7 @@ const multer = require("multer");
 const path = require("path");
 const subcategoryController = require("../Controllers/SubCategoryController");
 
+
 // Create an Express router
 const router = express.Router();
 
