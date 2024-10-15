70a5142dca04d56338a15df03484bb0f5db1e37c
Ajit raut <ajitraut9561@gmail.com>
2024-10-15 15:18:52 +0530

    Modifying

--- Server/Models/Subcategory.js
+++ Server/Models/Subcategory.js
@@ -1,26 +1,29 @@
 const mongoose = require("mongoose");
 
-const subcategorySchema = new mongoose.Schema({
-  subcatname: {
-    type: String,
-    required: true,
+const subcategorySchema = new mongoose.Schema(
+  {
+    subcatname: {
+      type: String,
+      required: true,
+    },
+    categoryName: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ["active", "inactive"],
+      default: "active",
+    },
   },
-  categoryName: {
-    type: String,
-    required: true,
-  },
-  image: {
-    type: String,
-    required: true,
-  },
-  status: {
-    type: String,
-    enum: ["active", "inactive"],
-    default: "active",
-  },
-}, {
-  timestamps: true,
-});
+  {
+    timestamps: true,
+  }
+);
 
 const Subcategory = mongoose.model("Subcategory", subcategorySchema);
 
