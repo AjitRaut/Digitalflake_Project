61c51ad446a62449f82105975c9d010a2ccc0b6b
Ajit raut <ajitraut9561@gmail.com>
2024-10-20 21:06:40 +0530

    updated

---
+++ Client/src/hooks/useGetSubcategories.jsx
@@ -0,0 +1,30 @@
+// hooks/useGetSubcategories.js
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { toast } from "react-toastify";
+
+const useGetSubcategories = () => {
+  const [subcategories, setSubcategories] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const fetchSubcategories = async () => {
+    try {
+      const response = await axios.get(
+        "http://localhost:5000/api/subcategories"
+      );
+      setSubcategories(response.data);
+      setLoading(false);
+    } catch (error) {
+      toast.error("Error fetching subcategories");
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchSubcategories();
+  }, []);
+
+  return { subcategories, loading };
+};
+
+export default useGetSubcategories;
