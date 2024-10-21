3f1e0a0429ad3deadf03bf7bcb65a207558743d7
Ajit raut <ajitraut9561@gmail.com>
2024-10-21 21:32:07 +0530

    Added useProducts

---
+++ Client/src/hooks/useProducts.jsx
@@ -0,0 +1,39 @@
+// hooks/useProducts.js
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { toast } from "react-toastify";
+
+const useProducts = () => {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const fetchProducts = async () => {
+    setLoading(true);
+    try {
+      const response = await axios.get("http://localhost:5000/api/products");
+      setProducts(response.data);
+    } catch (error) {
+      toast.error("Error fetching products");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const deleteProduct = async (id) => {
+    try {
+      await axios.delete(`http://localhost:5000/api/products/${id}`);
+      setProducts((prevProducts) => prevProducts.filter((product) => product._id !== id));
+      toast.success("Product deleted successfully");
+    } catch (error) {
+      toast.error("Error deleting product");
+    }
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  return { products, loading, fetchProducts, deleteProduct };
+};
+
+export default useProducts;
